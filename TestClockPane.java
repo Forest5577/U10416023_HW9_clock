4dc545eb282eb19374145127c4f5298e217bd157
Forest5577 <Forest5757@gmail.com>
2016-06-06 21:15:53 +0800

    Import Api
    Import api which is gonna use

---
+++ TestClockPane.java
@@ -0,0 +1,13 @@
+//U10416023
+
+//Import Api
+import javafx.util.Duration;
+import java.util.Timer;
+import javafx.event.*;
+import javafx.animation.*;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
